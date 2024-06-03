101294050d2f36608b0e5b1fa63e37db24e7e57b
andy alvia <andy.alvia.ing@outlook.com>
2024-06-02 23:28:17 -0500

    mmlhdlyec

--- src/components/movie/MovieDetails.tsx
+++ src/components/movie/MovieDetails.tsx
@@ -193,7 +193,7 @@ const AuthButton: React.FC<{ editMode: boolean, handleReviewSubmit: () => void, 
           <FiX className="mr-1" /> Cancelar
         </button>
       ) : (
-        <div></div> // No muestra nada si no está en modo de edición
+        <div></div> 
       )}
       <button
         className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
